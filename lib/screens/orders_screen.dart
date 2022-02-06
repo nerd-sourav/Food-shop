b06feb3ec1c8940c4919bf1abdddffc5c131e7cf
nerd-sourav <souravprasad1869@gmail.com>
2022-02-06 20:19:50 +0530

    changes in the assets

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/provider/orders.dart';
+
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Orders'),
+      ),
+      body: ListView.builder(
+        itemCount: orderData.orders.length,
+        itemBuilder: (BuildContext context, int index) {
+          return;
+        },
+      ),
+    );
+  }
+}
---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:shop/provider/orders.dart' as ord;
+
+class OrderItem extends StatelessWidget {
+  final ord.OrderItem order;
+  const OrderItem({
+    Key? key,
+    required this.order,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('\$${order.amount}'),
+            subtitle:   ,
+          )
+        ],
+      ),
+    );
+  }
+}
